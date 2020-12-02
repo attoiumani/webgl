3ec91a9d4dbd8217318c6cbc9d60b456cac89570
shirakawa <attoiumani0313@gmail.com>
2020-12-02 14:01:25 +0900

    Create About.vue

---
+++ src/views/About.vue
@@ -0,0 +1,47 @@
+<template>
+  <canvas id="canvas"></canvas>
+</template>
+
+<script>
+import * as PIXI from "pixi.js";
+import { GlitchFilter } from "pixi-filters";
+
+export default {
+  data() {
+    return {
+      imgPath: require(`@/assets/cr7.jpg`),
+    };
+  },
+
+  mounted() {
+    const app = new PIXI.Application({
+      //view:canvas
+      });
+  
+    document.body.appendChild(app.view);
+    app.stage.interactive = true;
+
+    const container = new PIXI.Container();
+    app.stage.addChild(container);
+
+    const glitchFilter = new GlitchFilter({
+      slices: 10,
+      offset: 100,
+      fillMode: 1,
+      speed: 0,
+    });
+
+    const displacementSprite = PIXI.Sprite.from(this.imgPath);
+
+    displacementSprite.filters = [glitchFilter];
+
+    app.ticker.maxFPS = 1;
+    app.ticker.add(function () {
+      glitchFilter.offset = Math.floor(Math.random() * 100);
+      glitchFilter.slices = Math.floor(Math.random() * 10);
+    });
+
+    app.stage.addChild(displacementSprite);
+  },
+};
+</script>
