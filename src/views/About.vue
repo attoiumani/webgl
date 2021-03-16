1f5cb9f13042746515e121fd6cf68ab720ae9d89
shirakawa-web <74357158+shirakawa-web@users.noreply.github.com>
2021-03-16 14:04:25 +0900

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -18,7 +18,7 @@ export default {
   data() {
     return {
        imgPath: require(`@/assets/images/2019/messi2019.jpg`),
-      imgPath1: require(`@/assets/images/2019/2.jpg`),
+       imgPath1: require(`@/assets/images/2019/2.jpg`),
     };
   },
 
